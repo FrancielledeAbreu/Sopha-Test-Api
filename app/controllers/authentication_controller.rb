584758992becfba1741cf6d071355e0347eeba53
FRANCIELLE DE ABREU <francielle@alexandria.solar>
2023-09-23 09:28:54 -0300

    feat: Authentication controller created

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,34 @@
+class AuthenticationController < ApplicationController
+  skip_before_action :authorized
+  def register
+    @user = User.new(user_params)
+
+    if @user.save
+      token = JWT.encode({ user_id: @user.id }, 'your_secret', 'HS256')
+      render json: { token: token, user: { id: @user.id, email: @user.email, name: @user.name } }, status: :created
+    else
+      render json: { error: 'Failed to create user' }, status: :unprocessable_entity
+    end
+  end
+
+  def login
+    user = User.find_by(email: params[:email])
+
+    if user&.authenticate(params[:password])
+      token = encode_token({ user_id: user.id })
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid credentials' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:name, :email, :password)
+  end
+
+  def encode_token(payload)
+    JWT.encode(payload, 'your_secret')
+  end
+end
